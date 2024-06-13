61b9e5f8316a0850f51839c4825a38f712ed96f5
The Alpha <contact@nabilalsaiad.com>
2024-06-13 19:01:07 +0800

    Move visualisation to helper.py

--- src/dataset2/generator.py
+++ src/dataset2/generator.py
@@ -91,56 +91,7 @@ def main():
 	unique_routes = sorted(unique_routes)
 
 	write_file(stars)
-
-	G = nx.Graph()
-
-	for star in stars:
-		G.add_node(star.name, x=star.x, y=star.y, z=star.z, weight=star.weight, profit=star.profit)
-
-	for route in unique_routes:
-		i = route[0]
-		j = route[1]
-		s1 = stars[i]
-		s2 = stars[j]
-		G.add_edge(s1.name, s2.name, weight=(s1.weight + s2.weight))
-
-	# convert_node_labels_to_integers(G, first_label=0, ordering='default', label_attribute=star)
-
-	# print(f"number_of_nodes: {G.number_of_nodes()}")
-	# print(f"nodes: {list(G.nodes)}")
-	# print(f"number_of_edges: {G.number_of_edges()}")
-	# print(f"edges: {list(G.edges)}")
-
-	pos = {star.name: (star.x, star.y) for star in stars}
-
-	# # Customize node size, node color, and node shape
-	# node_sizes = [star.weight * 10 for star in stars]  # Adjust size multiplier as needed
-	# node_colors = range(len(stars))
-	# nx.draw_networkx_nodes(G, pos, node_size=node_sizes, node_color=node_colors, node_shape='s')
-
-	# # Customize edge width and edge color
-	# edge_widths = [G[u][v]['weight'] / 10 for u, v in G.edges()]  # Adjust width divisor as needed
-	# edge_colors = range(len(G.edges()))
-	# nx.draw_networkx_edges(G, pos, width=edge_widths, edge_color=edge_colors)
-
-	# # Draw nodes
-	# node_sizes = [star.weight * 10 for star in stars]  # Adjust size multiplier as needed
-	# nx.draw_networkx_nodes(G, pos, node_size=node_sizes, node_color='blue', node_shape='s')
-
-	# # Draw edges
-	# edge_widths = [G[u][v]['weight'] / 10 for u, v in G.edges()]  # Adjust width divisor as needed
-	# nx.draw_networkx_edges(G, pos, width=edge_widths, edge_color='black')
-
-	# # Draw node labels with weights
-	# node_labels = {node: data['weight'] for node, data in G.nodes(data=True)}
-	# nx.draw_networkx_labels(G, pos, labels=node_labels)
-
-	# # Draw edge labels with weights
-	# edge_labels = nx.get_edge_attributes(G, 'weight')
-	# nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-
-	nx.draw(G, pos=pos, with_labels=True)
-	plt.show()
+	# draw_graph(stars, unique_routes)
 
 if __name__ == '__main__':
 	main()
